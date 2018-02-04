0eb7d38e35d9c32e9d00b526768d998c542ad6bb
a8\pax <es700x@yahoo.com>
2018-02-04 06:18:06 -0600

    version .1 Small batch util to isntall apk to many different devices connected by usb. Android.

---
+++ manyDevices.bat
@@ -0,0 +1,15 @@
+setlocal EnableDelayedExpansion           
+for /f "tokens=*" %%f in ('C:\Users\franMX\AppData\Local\Android\android-sdk\platform-tools\adb.exe devices') do (
+
+		set devicestr=%%f
+		set apk=%%p
+		if "!devicestr!"=="!devicestr:List=!" (
+			for /f "tokens=1" %%d in ("!devicestr!") do (
+				set deviceid=%%d
+				echo !deviceid!
+				echo !apk!
+				C:\Users\franMX\AppData\Local\Android\android-sdk\platform-tools\adb.exe -s !deviceid! install app.apk
+			)
+		)
+
+)
